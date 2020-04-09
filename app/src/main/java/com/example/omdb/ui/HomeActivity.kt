99f34676bd37744bb91069605ead18ce24d4231e
Pranit Rane <rane.pranit.b@gmail.com>
2020-04-10 03:44:22 +0530

    update: theme 1. day/night support 2. ripple effects for card, text buttons, image buttons

--- app/src/main/java/com/example/omdb/ui/HomeActivity.kt
+++ app/src/main/java/com/example/omdb/ui/HomeActivity.kt
@@ -3,7 +3,9 @@ package com.example.omdb.ui
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.omdb.R
+import com.example.omdb.databinding.ActivityHomeBinding
 import com.example.omdb.util.NotificationChannels
 import dagger.android.support.DaggerAppCompatActivity
 
@@ -14,13 +16,14 @@ class HomeActivity : DaggerAppCompatActivity() {
     }
 
     //Global
+    private lateinit var binding: ActivityHomeBinding
     private val manager by lazy { getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
         NotificationChannels.create(manager, resources)
     }
