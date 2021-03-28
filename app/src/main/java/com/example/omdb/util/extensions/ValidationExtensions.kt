c721613ef9195556a5d2e51193abc1c524175a8e
Pranit Rane <pranitrane619@gmail.com>
2021-03-29 03:36:11 +0530

    update: misc changes
    1. moved transitions to utils
    2. replaced android email pattern with androidx email pattern

--- app/src/main/java/com/example/omdb/util/extensions/ValidationExtensions.kt
+++ app/src/main/java/com/example/omdb/util/extensions/ValidationExtensions.kt
@@ -1,6 +1,8 @@
 package com.example.omdb.util.extensions
 
+import androidx.core.util.PatternsCompat.EMAIL_ADDRESS
+
 fun String?.isValidEmail(): Boolean {
     val email = this?.trim() ?: return false
-    return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    return EMAIL_ADDRESS.matcher(email).matches()
 }
