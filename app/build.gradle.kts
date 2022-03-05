5ef98c7b67b677a4edd3b57b580203cc20b3611d
Pranit Rane <pranitrane619@gmail.com>
2022-03-05 16:45:24 +0530

    upgrade: dependencies
    1. gradle
    2. room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -140,11 +140,11 @@ dependencies {
     kapt("com.github.bumptech.glide:compiler:4.12.0")
 
     // Room
-    implementation("androidx.room:room-runtime:2.4.1")
-    kapt("androidx.room:room-compiler:2.4.1")
+    implementation("androidx.room:room-runtime:2.4.2")
+    kapt("androidx.room:room-compiler:2.4.2")
 
     // Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.4.1")
+    implementation("androidx.room:room-ktx:2.4.2")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
