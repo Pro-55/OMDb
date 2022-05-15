ad36f75666b872b42f9a06020a0dffea869a4282
Pranit Rane <pranitrane619@gmail.com>
2022-05-15 07:14:10 +0530

    update: response class
    1. response sealed class added
    2. constants updated

---
+++ app/src/main/java/com/example/omdb/models/network/Response.kt
@@ -0,0 +1,12 @@
+package com.example.omdb.models.network
+
+import com.example.omdb.util.Constants
+
+sealed class Response<T>(val msg: String? = null, val data: T? = null) {
+    class Success<T>(data: T?) : Response<T>(data = data)
+    class UnknownHostException<T> : Response<T>()
+    class InvalidPathException<T> : Response<T>(msg = Constants.ERROR_MESSAGE_INVALID_PATH)
+    class InvalidRequestException<T> : Response<T>(msg = Constants.ERROR_MESSAGE_INVALID_REQUEST)
+    class ServerException<T> : Response<T>(msg = Constants.ERROR_MESSAGE_SERVER_EXCEPTION)
+    class UnknownException<T> : Response<T>(msg = Constants.ERROR_MESSAGE_UNKNOWN)
+}
--- app/src/main/java/com/example/omdb/util/Constants.kt
+++ app/src/main/java/com/example/omdb/util/Constants.kt
@@ -17,14 +17,20 @@ object Constants {
 
     const val REQUEST_GOOGLE_SIGN_IN = 0
 
-    const val BULLET = "\u2022"
-    const val GHOST_EMOJI = "\uD83D\uDC7B"
-    const val THINKING_EMOJI = "\uD83E\uDD14"
+    private const val EMOJI_MONOCLE_FACE = "\uD83E\uDDD0"
+    private const val EMOJI_TURTLE = "\uD83D\uDC22"
+    private const val EMOJI_EXPLODING_HEAD = "\uD83E\uDD2F"
+    private const val EMOJI_SPIRAL_EYES = "\uD83D\uDE35\u200D\uD83D\uDCAB"
+    private const val THINKING_EMOJI = "\uD83E\uDD14"
     const val RISING_SUN_EMOJI = "\uD83C\uDF04"
     const val SUN_EMOJI = "\u26C5"
     const val SETTING_SUN_EMOJI = "\uD83C\uDF06"
     const val CRESCENT_MOON_EMOJI = "\uD83C\uDF19"
-    const val NETWORK_ERROR_MESSAGE = "Oops, your connection seems off... $GHOST_EMOJI"
     const val REQUEST_FAILED_MESSAGE = "Something went wrong... $THINKING_EMOJI"
+    const val ERROR_MESSAGE_INVALID_PATH = "We couldn't find the data... $EMOJI_SPIRAL_EYES"
+    const val ERROR_MESSAGE_INVALID_REQUEST = "Failed to process the request... $EMOJI_MONOCLE_FACE"
+    const val ERROR_MESSAGE_SERVER_EXCEPTION =
+        "Servers are down! Please try again later... $EMOJI_TURTLE"
+    const val ERROR_MESSAGE_UNKNOWN = "Something went wrong... $EMOJI_EXPLODING_HEAD"
     const val NOT_AVAILABLE = "N/A"
 }
