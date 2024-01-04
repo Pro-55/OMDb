33af67cf4e945b039f390dfa24418a9a40bc63b8
Pranit Rane <pranitrane619@gmail.com>
2024-01-04 18:46:51 +0530

    upgrade: dependencies
    1. gradle
    2. kotlin
    3. hilt
    4. navigation safeargs
    5. google services
    6. firebase
    7. androidx core
    8. material
    9. androidx lifecycle
    10. fragment
    11. naviagtion
    12. recycler view
    13. kotlinx serialization
    14. ktor
    15. glide
    16. room
    17. coroutines
    18. gradle version management updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath(kotlin(module = "gradle-plugin", version = "1.8.0"))
-        classpath(kotlin(module = "serialization", version = "1.8.10"))
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0")
-        classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.7")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks {
-    val clean by registering(Delete::class) {
-        delete(buildDir)
-    }
+plugins {
+    alias(libs.plugins.android.gradle) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.navigation.safeargs) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
 }
